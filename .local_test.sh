104097b8a4b4302230fc704299c8c7614a0c50f7
Matt Curfman <matt@tecnovelty.com>
2018-07-08 18:32:53 +0000

    Small fixes to support testing with ROS indigo and melodic (Only Kinetic is officially supported)

--- .local_test.sh
+++ .local_test.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+docker run --rm -i -v `pwd`:`pwd` -e "CI_SOURCE_PATH=`pwd`" -e "ROS_DISTRO=indigo" -t ubuntu:trusty sh -c "cd `pwd`; ./.travis.sh"
 docker run --rm -i -v `pwd`:`pwd` -e "CI_SOURCE_PATH=`pwd`" -e "ROS_DISTRO=kinetic" -t ubuntu:xenial sh -c "cd `pwd`; ./.travis.sh"
+docker run --rm -i -v `pwd`:`pwd` -e "CI_SOURCE_PATH=`pwd`" -e "ROS_DISTRO=melodic" -t ubuntu:bionic sh -c "cd `pwd`; ./.travis.sh"
--- .travis.sh
+++ .travis.sh
@@ -4,12 +4,12 @@ set -e
 export DEBIAN_FRONTEND noninteractive
 export TERM xterm
 
-apt-get update && apt-get install -y -q wget sudo lsb-release # for docker 
+apt-get update && apt-get install -y -q wget sudo lsb-release gnupg
 
 #before_install:
 sh -c "echo \"deb http://packages.ros.org/ros/ubuntu `lsb_release -sc` main\" > /etc/apt/sources.list.d/ros-latest.list"
-wget http://packages.ros.org/ros.key -O - | apt-key add -
-apt-get update && apt-get install -y -q python-catkin-pkg python-rosdep python-wstool ros-kinetic-catkin build-essential
+sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+apt-get update && apt-get install -y -q python-catkin-pkg python-rosdep python-wstool ros-$ROS_DISTRO-catkin build-essential
 source /opt/ros/$ROS_DISTRO/setup.bash
 rosdep init
 rosdep update
@@ -18,10 +18,6 @@ rosdep update
 mkdir -p ~/catkin_ws/src
 cd ~/catkin_ws/src
 catkin_init_workspace
-cd ~/catkin_ws
-catkin_make
-source devel/setup.bash
-cd ~/catkin_ws/src
 ln -s $CI_SOURCE_PATH .
 
 #before_script:
